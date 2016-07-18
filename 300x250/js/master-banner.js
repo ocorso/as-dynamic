193dc1fd5b8ae304af32ddce51142fa027a0590f
Owen Corso <owen@ored.net>
2016-07-18 10:36:49 -0400

    getting team up and running.

--- 300x250/js/master-banner.js
+++ 300x250/js/master-banner.js
@@ -1,7 +1,29 @@
+ if (!Enabler.isInitialized()) {
+  Enabler.addEventListener(
+    studio.events.StudioEvent.INIT,
+    enablerInitialized);
+} else {
+   enablerInitialized();
+}
+function enablerInitialized() {
+  // Enabler initialized.
+  // In App ads are rendered offscreen so animation should wait for
+  // the visible event. These are simulated with delays in the local
+  // environment.
+  if (!Enabler.isVisible()) {
+    Enabler.addEventListener(
+      studio.events.StudioEvent.VISIBLE,
+      adVisible);
+  } else {
+     adVisible();
+  }
+}//end function enabler init();
+
+
 function adVisible() {
   console.log('adVisible();');
 
-  
+
 
 };
 
