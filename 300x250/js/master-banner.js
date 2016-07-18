c19808f3db03dccbf1db3a0dd72f56aef624ef08
Owen Corso <owen@ored.net>
2016-07-18 11:19:58 -0400

    Created switch and documented accidentForgivenessInit

--- 300x250/js/master-banner.js
+++ 300x250/js/master-banner.js
@@ -1,3 +1,6 @@
+ var asDynamic = {};
+
+
  if (!Enabler.isInitialized()) {
   Enabler.addEventListener(
     studio.events.StudioEvent.INIT,
@@ -13,17 +16,58 @@ function enablerInitialized() {
   if (!Enabler.isVisible()) {
     Enabler.addEventListener(
       studio.events.StudioEvent.VISIBLE,
-      adVisible);
+      asDynamic.init);
   } else {
-     adVisible();
+     asDynamic.init();
   }
 }//end function enabler init();
 
+/**
+ * This function is called when the Enabler is initialized.
+ * Uses dynamic content to determine which variation to display
+ * 
+*/
+asDynamic.init = function () {
+  console.log('asDynamic.init();');
 
-function adVisible() {
-  console.log('adVisible();');
+  //oc: switch statement goes here.
+  switch ('accidentForgiveness1'){//oc: placeholder for now
+    case 'accidentForgiveness1': 
+      asDynamic.accidentForgivenessInit('option1');
 
+      break;
+    default : console.log('run default ad');
+  }//end switch
+};//end asDynamic.init
 
+/**
+  * This function handles the Accident Forgiveness variations
+  * @param - ad variation option 
+  * Global syles:
+      - Background: #8b86ca
+      - CTA:    “GET IN”
+  * Keyframes : 
+      1 : Logo Lockup
+      2 : 
+        Top-aligned paragraph - 
+        Font size: 36 px
+        Copy: “GET IN ON THIS, [MARKET NAME]”
+      3 : Photo w/ ⅓ - top White Copy
+        Copy : “FORGIVENESS”
+      4 : Photo w/ ⅓ - bottom white copy
+        Copy : “EVEN IF IT’S YOUR FAULT”
+      5 : End Frame 
+        OPTION ONE:
+          Top-aligned large copy - 36 px
+          Larger copy: “ACCIDENT FORGIVENESS”
+          Smaller copy - 18px
+            “Connect with a [Market Name] Allstate Agent”
+        OPTION TWO:
+          Top-aligned large copy - 36 px
+          Larger opy: “ACCIDENT FORGIVENESS”
+          Smaller copy - 18px
 
-};
-
+*/
+asDynamic.accidentForgivenessInit = function($option){
+  console.log('accidentForgivenessInit: option: '+ $option);
+}
