f16e00896ca4345bbbaa51e8d16a70fe95bc6bef
“Sheila <runningsheila@gmail.com>
2016-07-26 12:10:04 -0500

    fixed sync errors

--- 300x250/js/master.js
+++ 300x250/js/master.js
@@ -8,59 +8,7 @@
 */
 
 // Dynamic Content variables and sample values
-<<<<<<< Updated upstream
-Enabler.setProfileId(1085016);
-var devDynamicContent = {};
 
-devDynamicContent.aslocalfeed_Allstate_Local_Feed = [{}];
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0]._id = 0;
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Variation = "DW"; //oc: hardcode to variation for dev
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Is_Default = true;
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Exit_URL = {};
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Exit_URL.Url = "https://www.onallstate.com/LP/2016/brand-campaign/localagents/";
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].elemType = "LI ID";
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Savings_Percentage = "N\/A";
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Market_Name = "Aon Sample Market Name";
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Legal_Copy = "Savings &amp; coverage subject to terms, conditions and availability. Savings Vary. Allstate Indemnity Co. &amp; Allstate Fire and Casualty Insurance Co. &amp; their Affiliates: Northbrook, IL. &copy;2015 Allstate Insurance Co.";
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Background_Color = "#FF9E16";
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Image1_160x600 = {};
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Image1_160x600.Type = "file";
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Image1_160x600.Url = "https://s0.2mdn.net/ads/richmedia/studio/44448670/37231879_20160721142255788_DF_image1_300x250.jpg";
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Image2_160x600 = {};
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Image2_160x600.Type = "file";
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Image2_160x600.Url = "https://s0.2mdn.net/ads/richmedia/studio/44448670/37231879_20160721142255788_DF_image1_300x250.jpg";
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Image3_160x600 = {};
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Image3_160x600.Type = "file";
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Image3_160x600.Url = "https://s0.2mdn.net/ads/richmedia/studio/44448670/37231879_20160721142255788_DF_image1_300x250.jpg";
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Image1_300x250 = {};
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Image1_300x250.Type = "file";
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Image1_300x250.Url = "https://s0.2mdn.net/ads/richmedia/studio/44448670/37231879_20160721142255788_DF_image1_300x250.jpg";
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Image2_300x250 = {};
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Image2_300x250.Type = "file";
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Image2_300x250.Url = "https://s0.2mdn.net/ads/richmedia/studio/44448670/37231879_20160721142255788_DF_image1_300x250.jpg";
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Image3_300x250 = {};
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Image3_300x250.Type = "file";
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Image3_300x250.Url = "https://s0.2mdn.net/ads/richmedia/studio/44448670/37231879_20160721142255788_DF_image1_300x250.jpg";
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Image1_728x90 = {};
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Image1_728x90.Type = "file";
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Image1_728x90.Url = "https://s0.2mdn.net/ads/richmedia/studio/44448670/37231879_20160721142255788_DF_image1_300x250.jpg";
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Image2_728x90 = {};
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Image2_728x90.Type = "file";
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Image2_728x90.Url = "https://s0.2mdn.net/ads/richmedia/studio/44448670/37231879_20160721142255788_DF_image1_300x250.jpg";
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Image3_728x90 = {};
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Image3_728x90.Type = "file";
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Image3_728x90.Url = "https://s0.2mdn.net/ads/richmedia/studio/44448670/37231879_20160721142255788_DF_image1_300x250.jpg";
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Image1_300x600 = {};
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Image1_300x600.Type = "file";
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Image1_300x600.Url = "https://s0.2mdn.net/ads/richmedia/studio/44448670/37231879_20160721142255788_DF_image1_300x250.jpg";
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Image2_300x600 = {};
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Image2_300x600.Type = "file";
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Image2_300x600.Url = "https://s0.2mdn.net/ads/richmedia/studio/44448670/37231879_20160721142255788_DF_image1_300x250.jpg";
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Image3_300x600 = {};
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Image3_300x600.Type = "file";
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Image3_300x600.Url = "https://s0.2mdn.net/ads/richmedia/studio/44448670/37231879_20160721142255788_DF_image1_300x250.jpg";
-Enabler.setDevDynamicContent(devDynamicContent);
-=======
     Enabler.setProfileId(1085016);
     var devDynamicContent = {};
 
@@ -112,7 +60,7 @@ Enabler.setDevDynamicContent(devDynamicContent);
     devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Image3_300x600.Type = "file";
     devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Image3_300x600.Url = "https://s0.2mdn.net/ads/richmedia/studio/44448670/37231879_20160721142255788_DF_image1_300x250.jpg";
     Enabler.setDevDynamicContent(devDynamicContent);
->>>>>>> Stashed changes
+
 
 /**
  * You may access the variables in the following manner
@@ -232,83 +180,6 @@ asApp.init = function() {
 
   //oc: Step 4: handle variations independently.
   switch (dynamicContent.aslocalfeed_Allstate_Local_Feed[0].Variation) { //oc: hardcode variation for dev line 16
-<<<<<<< Updated upstream
-    case asApp.type.AF:
-      asApp.accidentForgivenessInit();
-      break;
-    case asApp.type.BSAHLP:
-      asApp.bundleSaveInitAHLP();
-      break;
-    case asApp.type.BSAHL:
-      asApp.bundleSaveInitAHL();
-      break;
-    case asApp.type.BSHLI:
-      asApp.bundleSaveInitHLI();
-      break;
-    case asApp.type.BSALP:
-      asApp.bundleSaveInitALP();
-      break;
-    case asApp.type.BSAL:
-      asApp.bundleSaveInitAL();
-      break;
-    case asApp.type.BSAHMP:
-      asApp.bundleSaveInitAHMP();
-      break;
-    case asApp.type.BSAHM:
-      asApp.bundleSaveInitAHM();
-      break;
-    case asApp.type.BSACP:
-      asApp.bundleSaveInitACP();
-      break;
-    case asApp.type.BSAC:
-      asApp.bundleSaveInitAC();
-      break;
-    case asApp.type.CFD:
-      asApp.claimFreeDiscountInit();
-      break;
-    case asApp.type.CFDM:
-      asApp.claimFreeDiscountInit('mob');
-      break;
-    case asApp.type.CFR:
-      asApp.claimFreeRewardsInit();
-      break;
-    case asApp.type.CRG:
-      asApp.claimRateGuardInit();
-      break;
-    case asApp.type.CRGM:
-      asApp.claimRateGuardInit('mob');
-      break;
-    case asApp.type.CSG:
-      asApp.claimSatGuardInit();
-      break;
-    case asApp.type.CSGM:
-      asApp.claimSatGuardInit('mob');
-      break;
-    case asApp.type.DW:
-      asApp.driveWiseInit();
-      break;
-    case asApp.type.DW2P:
-      asApp.driveWiseInit();
-      break;
-    case asApp.type.NR:
-      asApp.newRoofInit();
-      break;
-    case asApp.type.TA1:
-      asApp.trustedAdvisorInit('opt1');
-      break;
-    case asApp.type.TAM1:
-      asApp.trustedAdvisorInit('mobopt1');
-      break;
-    case asApp.type.TAM2:
-      asApp.trustedAdvisorInit('mobopt2');
-      break;
-    case asApp.type.WD:
-      asApp.welcomeDiscountInit();
-      break;
-    case asApp.type.DF:
-      asApp.defaultInit();
-      break;
-=======
     case asApp.type.AF : asApp.accidentForgivenessInit(); break;
     case asApp.type.BSAHLP : asApp.bundleSaveInitAHLP(); break;
     case asApp.type.BSAHL : asApp.bundleSaveInitAHL(); break;
@@ -334,7 +205,6 @@ asApp.init = function() {
     case asApp.type.TAM2 : asApp.trustedAdvisorInit('mobopt2'); break;
     case asApp.type.WD : asApp.welcomeDiscountInit(); break;
     case asApp.type.DF : asApp.defaultInit(); break;
->>>>>>> Stashed changes
     default:
       console.log('Run default ad');
       asApp.defaultInit();
