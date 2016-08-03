733a75777dfe566bf416cb0d05bd6446d1f14192
“Sheila <runningsheila@gmail.com>
2016-08-03 16:44:38 -0500

    altered timing

--- 300x600/js/master.js
+++ 300x600/js/master.js
@@ -14,7 +14,7 @@ var devDynamicContent = {};
 devDynamicContent.aslocalfeed_Allstate_Local_Feed = [{}];
 devDynamicContent.aslocalfeed_Allstate_Local_Feed[0]._id = 0;
 
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Variation = "WD"; //oc: hardcode to variation for dev
+devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Variation = "DF"; //oc: hardcode to variation for dev
 devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Is_Default = true;
 devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Exit_URL = {};
 devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Exit_URL.Url = "https://www.onallstate.com/LP/2016/brand-campaign/localagents/";
@@ -787,12 +787,8 @@ asApp.addBS3FramesToTimeline = function($variation) {
     ease: Sine.easeOut
   });
 
-  tl.staggerFrom(frame3.words, 0.2, {
-    left: -300,
-  }, 0.07)
-
-  tl.staggerFrom(frame3a.words, 0.2, {
-    left: -300,
+  tl.staggerFrom( [frame3.lines,frame3a.lines], 0.2, {
+    left: -728,
   }, 0.07)
 
   tl.to("#bgImg2", 0.5, {
@@ -834,12 +830,8 @@ asApp.addBS2FramesToTimeline = function($variation) {
     ease: Sine.easeOut
   });
 
-  tl.staggerFrom(frame3.words, 0.2, {
-    left: -300
-  }, 0.07)
-
-  tl.staggerFrom(frame3a.words, 0.2, {
-    left: -300,
+  tl.staggerFrom( [frame3.lines,frame3a.lines], 0.2, {
+    left: -728,
   }, 0.07)
 
   tl.staggerFrom(frame4.words, 0.2, {
