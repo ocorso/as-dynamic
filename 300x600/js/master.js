066107911fe6d9785d72a9b6b940819e61779776
Whitt, Chad <cwht9@allstate.com>
2016-08-04 06:44:43 -0500

    Adjustments to fit text in (long market names)

--- 300x600/js/master.js
+++ 300x600/js/master.js
@@ -14,13 +14,13 @@ var devDynamicContent = {};
 devDynamicContent.aslocalfeed_Allstate_Local_Feed = [{}];
 devDynamicContent.aslocalfeed_Allstate_Local_Feed[0]._id = 0;
 
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Variation = "DF"; //oc: hardcode to variation for dev
+devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Variation = "CSG"; //oc: hardcode to variation for dev
 devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Is_Default = true;
 devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Exit_URL = {};
 devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Exit_URL.Url = "https://www.onallstate.com/LP/2016/brand-campaign/localagents/";
 devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].elemType = "LI ID";
 devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Savings_Percentage = "33%";
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Market_Name = "Greater Rochester Area";
+devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Market_Name = "Massachusetts Area";
 devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Legal_Copy = "Savings &amp; coverage subject to terms, conditions and availability. Savings Vary. Allstate Indemnity Co. &amp; Allstate Fire and Casualty Insurance Co. &amp; their Affiliates: Northbrook, IL. &copy;2015 Allstate Insurance Co.";
 devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Background_Color = "#FF9E16";
 devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Image1_160x600 = {};
@@ -787,7 +787,7 @@ asApp.addBS3FramesToTimeline = function($variation) {
     ease: Sine.easeOut
   });
 
-  tl.staggerFrom( [frame3.lines,frame3a.lines], 0.2, {
+  tl.staggerFrom([frame3.lines, frame3a.lines], 0.2, {
     left: -728,
   }, 0.07)
 
@@ -830,7 +830,7 @@ asApp.addBS2FramesToTimeline = function($variation) {
     ease: Sine.easeOut
   });
 
-  tl.staggerFrom( [frame3.lines,frame3a.lines], 0.2, {
+  tl.staggerFrom([frame3.lines, frame3a.lines], 0.2, {
     left: -728,
   }, 0.07)
 
