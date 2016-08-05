516278d7ccaf46631de92f296df9ae2fb99df19b
Whitt, Chad <cwht9@allstate.com>
2016-08-04 20:15:36 -0500

    The Greater Rochester Area

--- 728x90/js/master.js
+++ 728x90/js/master.js
@@ -14,13 +14,13 @@ var devDynamicContent = {};
 devDynamicContent.aslocalfeed_Allstate_Local_Feed = [{}];
 devDynamicContent.aslocalfeed_Allstate_Local_Feed[0]._id = 0;
 
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Variation = "CFD"; //oc: hardcode to variation for dev
+devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Variation = "TA1"; //oc: hardcode to variation for dev
 devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Is_Default = true;
 devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Exit_URL = {};
 devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Exit_URL.Url = "https://www.onallstate.com/LP/2016/brand-campaign/localagents/";
 devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].elemType = "LI ID";
 devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Savings_Percentage = "33%";
-devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Market_Name = "New York Area";
+devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Market_Name = "Greater Rochester Area";
 devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Legal_Copy = "Savings &amp; coverage subject to terms, conditions and availability. Savings Vary. Allstate Indemnity Co. &amp; Allstate Fire and Casualty Insurance Co. &amp; their Affiliates: Northbrook, IL. &copy;2015 Allstate Insurance Co.";
 devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Background_Color = "#FF9E16";
 devDynamicContent.aslocalfeed_Allstate_Local_Feed[0].Image1_160x600 = {};
@@ -518,6 +518,7 @@ asApp.splitText = function() {
       });
       //cw: For only Greater Rochester Area do this on frame5
       if ((dynamicContent.aslocalfeed_Allstate_Local_Feed[0].Variation == "TA1") && (dynamicContent.aslocalfeed_Allstate_Local_Feed[0].Market_Name == "Greater Rochester Area")) {
+        console.log("3423423423423423423423423423423423423423423423423");
         $('.frame5').html('in<br>the <span class="market-name">Greater Rochester Area</span>');
         frame5 = new SplitText(frame5Selector, {
           type: "lines,words",
